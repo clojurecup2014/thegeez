a7cea10863340fbca1fbddb780083ca6a917f163
thegeez <thegeez@users.noreply.github.com>
2014-09-27 21:19:31 +0200

    Drag targets

--- src/cljs/net/thegeez/datominoes/core.cljs
+++ src/cljs/net/thegeez/datominoes/core.cljs
@@ -9,7 +9,7 @@
 (enable-console-print!)
 
 (def app-state
-  {:stones 
+  {:stones
    (into {}
          (for [up (range 7)
                down (range up)]
@@ -17,7 +17,16 @@
              [id {:id id
                   :up up
                   :down down
-                  :orientation :vertical}])))})
+                  :orientation :vertical}])))
+   :places
+   {1 {:id 1
+       :left 380
+       :top 230
+       :orientation :horizontal}
+    2 {:id 2
+       :left 310
+       :top 150
+       :orientation :vertical}}})
 
 (def dot-mapping {0 [[0 0 0]
                      [0 0 0]
@@ -57,48 +66,51 @@
     (render-state [_ state]
       (apply dom/div {:className "faces"}
              (for [f [:up :down]]
-               (do (println "cursor" (om/value cursor) (:orientation cursor)) 
-                   (om/build face [f (get cursor f) (:orientation cursor)])))))))
+               (om/build face [f (get cursor f) (:orientation cursor)]))))))
 
 (defn stone [cursor owner]
   (reify
     om/IRender
     (render [_]
-      (let [orientation (or (om/get-state owner :orientation) :vertical)
+      (let [[id {:keys [top left orientation]}] cursor
             dragging (om/get-state owner :dragging)]
-        (println "Orientation: " orientation)
         (dom/div #js {:className (str "stone " (name orientation)
                                       (when dragging
                                         " dragging"))
                       :style (if dragging
                                #js {:border "3px solid black"
-                                    :top (om/get-state owner :top)
-                                    :left (om/get-state owner :left)}
+                                    :top top 
+                                    :left left }
                                #js {#_#_:border "3px solid green"
-                                    :top (om/get-state owner :top)
-                                    :left (om/get-state owner :left)})}
+                                    :top top
+                                    :left left })}
                  (om/build faces (-> (select-keys (val cursor) [:up :down])
                                      (assoc :orientation orientation))))))
     om/IDidMount
     (did-mount [_]
       (let [moves (async/chan (async/sliding-buffer 1))
-            out (async/chan)]
+            out (async/chan)
+            stone-id (first cursor)]
         (go-loop []
           (async/>! out (async/<! moves))
-          (async/<! (async/timeout 100))
+          (async/<! (async/timeout 16))
           (recur))
         (go-loop []
           (when-let [[x y] (async/<! out)]
             (when (om/get-state owner :dragging)
-              (om/update-state! owner
-                                (fn [s]
-                                  (assoc s
-                                    :orientation (if (< x 200)
-                                                   :vertical
-                                                   :horizontal
-                                                   )
-                                    :top (- y 20)
-                                    :left (- x 20)))))
+              (let [{:keys [top left orientation]} @(second cursor)
+                    top (- y (if (= orientation :vertical)
+                               40
+                               20))
+                    left (- x (if (= orientation :vertical)
+                                20
+                                40))]
+                (om/transact! (second cursor)
+                              (fn [s]
+                                (assoc s
+                                  :top top
+                                  :left left)))
+                (async/>! (om/get-shared owner :move-channel) stone-id)))
             (recur)))
         (let [node (om/get-node owner)]
           (gevents/listen node
@@ -120,12 +132,70 @@
                             (.preventDefault e)
                             (om/set-state! owner :dragging false))))))))
 
+(defn place [cursor owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [[id {:keys [top left orientation target]}] cursor]
+        (dom/div #js {:style #js {:position "absolute"
+                                  :top (- top 3)
+                                  :left (- left 3)
+                                  :border "3px solid blue"
+                                  :background (if target
+                                                "black"
+                                                "yellow")
+                                  :width (if (= orientation :vertical)
+                                           40 80)
+                                  :height (if (= orientation :vertical)
+                                            80 40)}}
+                 (str "open" (om/value cursor)))))))
+
+(defn overlap? [{:keys [orientation top left] :as stone} 
+                {place-orientation :orientation 
+                 place-top :top
+                 place-left :left
+                 :as place}]
+  (let [x (+ left (if (= orientation :vertical) 20 40))
+        y (+ top (if (= orientation :vertical) 40 20))
+        [dx dy] (if (= orientation :vertical)
+                  [40 80] [80 40])]
+    (and (< place-left x (+ place-left dx))
+         (< place-top y (+ place-top dy)))))
+
 (defn table [cursor owner]
   (reify
     om/IRender
     (render [_]
       (apply dom/div #js {:id "table"}
-             (om/build-all stone (:stones cursor))))))
+             (concat (om/build-all stone (:stones cursor))
+                     (om/build-all place (:places cursor) #_{:fn (fn [c]
+                                                                 (assoc-in c [1 :target] (:target cursor)))}))))
+    om/IDidMount
+    (did-mount [_]
+      (go-loop []
+        (when-let [stone-id (<! (om/get-shared owner :move-channel))]
+          (om/transact! cursor
+                        []
+                        (fn [cursor]
+                          (let [stone
+                                (get-in cursor [:stones stone-id])
+                                places (let [places (get-in cursor [:places])]
+                                         (zipmap (keys places)
+                                                 (map (fn [place]
+                                                        (assoc place :target
+                                                               (overlap? stone place)))
+                                                      (vals places))))
+                                to-orientation (some
+                                                (fn [[id place]]
+                                                  (when (:target place)
+                                                    (:orientation place)))
+                                                places)]
+                            (-> cursor
+                                (cond->
+                                 to-orientation
+                                 (assoc-in [:stones stone-id :orientation] to-orientation))
+                                (assoc :places places)))))
+          (recur))))))
 
 (defn footer [cursor owner]
   (reify
@@ -148,10 +218,10 @@
                (om/build footer nil)))))
 
 (defn ^:export main []
-  (println "Hello world")
   (om/root
    window
    app-state
-   {:target (.-body js/document)}))
+   {:target (.-body js/document)
+    :shared {:move-channel (async/chan)}}))
 
 
